858887d13708302370cdf2194910e30ea27d5b43
EsperanzaE <esperanza.berrocal@iesnervion.es>
2022-02-19 14:49:46 +0100

    versión final

--- src/TresEnRaya/Jugar.java
+++ src/TresEnRaya/Jugar.java
@@ -1,58 +1,70 @@
-
 package TresEnRaya;
-//El juego hará lo siguinete para lo que creará los métodos convenientes
+//El juego hará lo siguiente para lo que creará los métodos convenientes
 //Creará un clase tablero que estara formado por una matrix de 2x2 y el turno que inicia
 //cada celda de la matriz valdrá O,X o vacío. el jugador 1 rellenarán con 0 y el jugador 2 con X
 //en cada tirada se comprobará si existe alguna fila, o columna o la diagonal con el mismo contenido
 //en caso afirmativo se acabará el juego y se proclamará al vencedor. Si no hya vencedor se comprobará si el tablero
-//ya está lleno por que también se acabaría el juego sin declarar vencedor "tablas"
+//ya está lleno por que también se acabaría el juego sin declarar vencedor: "tablas"
 //se creará un menú para indicar si se quiere seguir jugando o desea parar
 //cada vez que un jugador juegue, se mostrará el estado del tablero
 
 
-import javax.management.ObjectInstance;
 import java.util.Scanner;
 
 public class Jugar {
-    public static Scanner sc=new Scanner(System.in);
+    private static final char NO = 'n';
+    private static final char SI = 's';
+    public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Tablero tablero=new Tablero();//creamos un tablero
-        boolean jugar=pedirOpcion();//pedimos al usuario si quiere jugar o no
-        boolean hayGanador=false, tableroLleno=false;
-        tablero.mostrar();//mostramos el tablero vacío
+        Tablero tablero = new Tablero();//creamos un tablero
+        boolean jugar = pedirOpcion();//pedimos al usuario si quiere jugar o no
+        boolean hayGanador = false, tableroLleno = false, finPartida = false;
 
-        while (jugar){//mientras quiera seguir jugando partidas
-        while (!tableroLleno && !hayGanador ){//mientras el tablero no esté lleno o no haya ningún ganador
+        while (jugar) {//mientras quiera seguir jugando partidas
+            tablero.mostrar();//mostramos el tablero
+            while (!finPartida) {////mientras el tablero no esté lleno o no haya ningún ganador
 
-            tablero.avisaAJugador(tablero.isJugador());//avisa al jugador para que mueva y recoge la opción
-            tablero.mueveJugador (tablero.isJugador());//El jugador mueve. Se valida que el movimiento sea válido
+                tablero.mueveJugador(tablero.isJugador());//avisa al jugador para que mueva y el jugador...
+                //...mueve. Se valida que el movimiento sea válido y se modifica el tablero
 
-            hayGanador=tablero.comprobarSiGanador();//se comprueba si con la última jugada hay algún ganador
-            if (!hayGanador) {
-                tableroLleno = tablero.comprobarSiLleno();//si no hay ganador se comprueba si el tablero ya está lleno
-                if (!tableroLleno){
-                    if (tablero.isJugador()){
-                        tablero.setJugador(false);
-                    }
-                    else {
-                        tablero.setJugador(true);
-                    }
+                finPartida = tablero.compruebaEstadoJuego(tablero.isJugador());//se comprueba si ha finalizado la partida
+                if (!finPartida) {
+                    tablero.mueveJugador(tablero.isJugador());//avisa al otro jugador para que mueva
+                    finPartida = tablero.compruebaEstadoJuego(tablero.isJugador());
                 }
             }
+            jugar = pedirOpcion();          //preguntamos  al usuario si quiere seguir jugando o no
+            if (jugar) {                    //si quiere seguir jugando se limpiar el tablero
+                tablero = new Tablero();
+                finPartida=false;
+            }
         }
-        jugar=pedirOpcion();
+        System.out.println("GAME OVER");
     }
-}
+
 
     private static boolean pedirOpcion() {
-        boolean jugar=false;
-        char teclado ='s';
-        System.out.println("Indique si quiere jugar al tres en raya: Sí/No");
-        try {
-            teclado=Character.toLowerCase(sc.nextLine().charAt(0));
-            if (teclado=='s')// --> por aqui me he quedado
+        System.out.println("Desea Jugar al tres en raya?, diga Sí o No");
+        boolean jugar = true, opcValida = false;
+        char teclado;
+        while (!opcValida) {
+            try {
+                teclado = Character.toLowerCase(sc.nextLine().charAt(0));
+                if (teclado == NO) {
+                    jugar = false;
+                    opcValida = true;
+                } else {
+                    if (teclado == SI) {
+                        opcValida = true;
+                    } else {
+                        System.out.println("introduzca Sí o No");
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("introduzca sí o no");
+            }
         }
-        catch (Exception e){
+        return jugar;
     }
-    }
+}
